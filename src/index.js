a7da7236141269237484883a4626a8718cf9ac60
natnaelh14 <natnaelh14@github.com>
2021-10-24 18:39:55 -0400

    integrated firebase

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { ConnectedRouter } from 'connected-react-router';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import { store, persistor, history } from './redux/store';
+
+import App from './app/App';
+
+import './index.css';
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
